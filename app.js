f8301f85b7e18122af740e8f57f3faa32915d60b
2003devansh <faujdardevansh@gmail.com>
2024-09-03 23:17:01 +0530

    make a condition regarding the selection of player ... {continue}

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ const server = http.createServer(app) ;
 const io  = socket(server) ;
 // instantiasing socket.io on http server
 
-const chess = new Chess() ; 
-
+const chess = new Chess() ;  
 let players = {} ; 
 let currentPlayer  = 'W' ;
 
@@ -33,16 +32,23 @@ app.get("/" , (req,res)=>{
 
 
 io.on("connection" ,(uniqueSocket)=>{
-    console.log("connected");
+    console.log("connected");  
 
-    uniqueSocket.on("can_be_anything",()=>{
-        console.log("heyy its responding");
-        io.emit("churan papdi")
-        // if data is receive from the fronend then emit the data from the backend to front-end        
-    }) ; 
-    // receive from the front-end 
-
-    
+    if(!players.white){
+        players.white  = uniqueSocket.id; 
+        uniqueSocket.emit("playerRole","w") ;
+    //  if there is none white player then create a white player 
+    // and give its unique id name :- uniqueSocket.id 
+    // and make a call to the front end that the player is been created
+    // and assingining the player role to the white 
+    }else if(!players.black){
+        players.black = uniqueSocket.id ;
+        uniqueSocket.emit("playerRole","b") ;
+    // if there is none black player then create a black player 
+    // and give its unique id name :- uniqueSocket.id 
+    // and make a call to the front-end that the player is been created
+    // and assinging its player  role to black .
+    }
 })
 
 server.listen(3000,()=>{
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,10 +1,2 @@
 // setting up socket.io in the front-end
 const socket = io();
-
-socket.emit("can_be_anything")
-// emitting something from front end to backend 
-
-socket.on("churan papdi" , ()=>{
-    console.log("data receive from the back-end");
-    
-})
