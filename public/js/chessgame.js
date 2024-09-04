965dec65caacce4cc9ef37ebba2958d7ee9cee88
2003devansh <faujdardevansh@gmail.com>
2024-09-04 23:11:19 +0530

    commiting

--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,49 @@
+
+
 // setting up socket.io in the front-end
 const socket = io();
 const chess = new Chess() ;
+const boardElement = document.querySelector("#chessboard") ;
+
+let draggedPiece = null ;
+let sourceSquare  = null ; 
+let player = null ;
+
+
+const renderBoard = () => {
+  const board  = chess.board();
+  boardElement.innerHTML = "" ; // making the board empty before  use
+  board.forEach((row,rowindex) => {
+    row.forEach((square,squareindex) =>{
+      const squareElement = document.createElement("div") ;
+      squareElement.classList.add(
+        "square",
+        (rowindex + squareindex) % 2 === 0 ? "light" : "dark" 
+      )
+      squareElement.dataset.row = rowindex ; 
+      squareElement.dataset.col = squareindex ;
+
+      if(square){
+        const pieceElement = document.createElement("div") ;
+        pieceElement.classList.add(
+            "piece" ,
+            square.color === "w" ? "white" : "black"
+        ); 
+        pieceElement = "" ; 
+        
+      }
+       
+    })
+    
+  });
+}
+
+const handleMove = () => {
+
+}
+
+const getPieceUnicode = () => {
+
+}
+
+renderBoard() ;
